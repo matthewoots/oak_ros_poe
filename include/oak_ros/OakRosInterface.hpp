05d2d0011efed4ac0535e244eb2aed0d16a0b19d
huiminc <cgz_05@sina.com>
2022-03-07 23:30:15 +0800

    rearrange documentation for interface

--- include/oak_ros/OakRosInterface.hpp
+++ include/oak_ros/OakRosInterface.hpp
@@ -13,6 +13,28 @@
 /**
  * @brief Oak Device parameters to configure output
  * 
+ * @param enable_stereo boolean to enable stereo images streaming (/{left,right}/image_rect_raw)
+ * @param enable_stereo_rectified boolean to stream rectified stereo, instead of `raw` stereo. if @enable_depth is false, this does nothing
+ * @param stereo_resolution optional parameter to specify the preferred stereo resolution
+ * @param stereo_fps optional parameter to specify the framerate
+ * @param stereo_fps_throttle optional parameter to specify framerate by using on-device script for frame dropping
+ * 
+ * @param enable_depth boolean to enable depth stream (TODO: not yet implemented)
+ * @param enable_depth_pointcloud TODO: not yet implemented
+ * 
+ * @param enable_rgb boolean to enable rgb stream (TODO: not yet implemented)
+ * @param rgb_resolution optional parameter to specify the preferred rgb resolution
+ * @param enable_imu boolean to enable imu stream
+ * @param imu_frequency the frequency IMU should sample upon
+ * 
+ * @param rates_workaround boolean to enable framerates halfing, on-device
+ * @param align_ts_to_right boolean to enable copy the exact ts of right camera to the left camera
+ * 
+ * @param manual_exposure optional parameter to set manual exposure
+ * @param manual_iso optional parameter to set manual iso, has to be used together with @manual_exposure
+ * 
+ * @param enable_apriltag boolean to enable apriltag detection streaming
+ * 
  * @note
  */
 
@@ -20,8 +42,9 @@ struct OakRosParams
 {
     std::string device_id;
     std::string topic_name = "oak";
+
     bool enable_stereo = true;
-    bool enable_stereo_rectified = true; // if enable_depth is false, then rectification will never happen
+    bool enable_stereo_rectified = true; 
     std::optional<dai::MonoCameraProperties::SensorResolution> stereo_resolution = {}; // dai::MonoCameraProperties::SensorResolution::THE_480_P
 
     std::optional<float> stereo_fps = {};
@@ -32,17 +55,18 @@ struct OakRosParams
     // dai::StereoDepthProperties
 
     bool enable_rgb = false;
-    bool enable_imu = false;
+    std::optional<dai::ColorCameraProperties::SensorResolution> rgb_resolution = {}; // dai::ColorCameraProperties::SensorResolution::THE_1080_P;
 
-    bool rates_workaround = false; // make image rates half using Movidius script
-    bool align_ts_to_right = true; // when enabled, the timestamp of the stereo is align to right camera's
-    
+    bool enable_imu = false;
     int imu_frequency = 200;
 
-    std::optional<dai::ColorCameraProperties::SensorResolution> rgb_resolution = {}; // dai::ColorCameraProperties::SensorResolution::THE_1080_P;
+    bool rates_workaround = false;
+    bool align_ts_to_right = true;
 
     std::optional<int> manual_exposure; // 1 - 33000
     std::optional<int> manual_iso; // 100 - 1600
+
+    bool enable_apriltag = false;
 };
 
 class OakRosInterface
