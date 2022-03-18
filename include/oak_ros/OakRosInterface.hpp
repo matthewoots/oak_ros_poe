f35500016c9e077258b4c3dd8a9a1374486e4d99
huiminc <cgz_05@sina.com>
2022-03-18 13:22:23 +0800

    add missing hpp file

--- include/oak_ros/OakRosInterface.hpp
+++ include/oak_ros/OakRosInterface.hpp
@@ -44,7 +44,8 @@ struct OakRosParams
     std::string topic_name = "oak";
 
     bool enable_stereo = true;
-    bool enable_stereo_rectified = true; 
+    bool enable_stereo_rectified = true;
+    std::string enable_mesh_dir; 
     std::optional<dai::MonoCameraProperties::SensorResolution> stereo_resolution = {}; // dai::MonoCameraProperties::SensorResolution::THE_480_P
 
     std::optional<float> stereo_fps = {};
