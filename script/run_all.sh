87e6bae09c3227d214d61045b4f84676a5078d9f
huiminc <cgz_05@sina.com>
2022-03-29 10:54:51 +0800

    add resolution option in run_all script

--- script/run_all.sh
+++ script/run_all.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-DRONE_NO="01"
+NAME="drone-01"
+RESOLUTION=480
 
 echo "STEP 1"
-python3 01-save_device_calibration.py depthai_calib_drone${DRONE_NO}.json
+python3 01-save_device_calibration.py depthai_calib_${NAME}.json
 
 echo "STEP 2"
-python3 02-flash_device_calibration.py depthai_calib_drone${DRONE_NO}.json camchain-drone${DRONE_NO}.yaml
+python3 02-flash_device_calibration.py depthai_calib_${NAME}.json camchain-${NAME}.yaml
 
 echo "STEP 3"
-python3 03-generate-mesh-file.py 
+python3 03-generate-mesh-file.py --resolution ${RESOLUTION}
 
-rm depthai_calib_drone${DRONE_NO}.json
+rm depthai_calib_${NAME}.json
