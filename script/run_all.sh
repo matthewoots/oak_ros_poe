56b8f73bad707df1be5909b0b273e4438a4d0d1d
huiminc <cgz_05@sina.com>
2022-04-05 14:30:39 +0800

    update calibration for S4 and S6

--- script/run_all.sh
+++ script/run_all.sh
@@ -2,14 +2,17 @@
 
 set -e
 
-NAME="drone-01"
+NAME="drone02"
+CALIB_TYPE="pinhole"
+
+# IMPORTANT: for OAK-D-PRO please use 400; for OAK-D-Lite please use 480
 RESOLUTION=480
 
 echo "STEP 1"
 python3 01-save_device_calibration.py depthai_calib_${NAME}.json
 
 echo "STEP 2"
-python3 02-flash_device_calibration.py depthai_calib_${NAME}.json camchain-${NAME}.yaml
+python3 02-flash_device_calibration.py depthai_calib_${NAME}.json camchain-${CALIB_TYPE}-${NAME}.yaml
 
 echo "STEP 3"
 python3 03-generate-mesh-file.py --resolution ${RESOLUTION}
